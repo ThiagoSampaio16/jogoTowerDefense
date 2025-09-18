3548a4beeea2481d3d7efa2b88aecff0cdc68d2b
Vítor.T <Martinsvitor.t@gmail.com>
2025-09-17 21:48:01 -0300

    Fiz a janela de abertura do jogo, inicialização pronta

---
+++ src/main/java/Jogo.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import javax.swing.JFrame;
+
+public class Jogo extends JFrame{
+
+    private TelaJogo telaJogo;
+
+    public Jogo(){
+        setSize(400, 400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        telaJogo = new TelaJogo();
+        add(telaJogo);
+    }
+public static void main(String[] args) {
+    Jogo jogo = new Jogo();
+
+}
+    
+}
