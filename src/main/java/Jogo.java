23bd9ee24da475c35f926ad4ddde263919550ec3
Vítor.T <Martinsvitor.t@gmail.com>
2025-10-06 11:14:25 -0300

    (Versão a corrigir)

--- src/main/java/Jogo.java
+++ src/main/java/Jogo.java
@@ -1,12 +1,21 @@
 package main.java;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Jogo extends JFrame{
 
     private TelaJogo telaJogo;
 
+    private BufferedImage img;
+
     public Jogo(){
+
+        importImg();
+
         setSize(640, 640);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -15,6 +24,14 @@ public class Jogo extends JFrame{
         telaJogo = new TelaJogo();
         add(telaJogo);
     }
+    private void importImg() {
+    InputStream is = getClass().getResourceAsStream("/Sprites_do_jogo_Larissa.png");
+    try {
+        img = ImageIO.read(is);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 public static void main(String[] args) {
     Jogo jogo = new Jogo();
 
